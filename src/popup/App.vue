dfcd2733c29b07aa64cd220b0f28ca3ad7749228
NoahYoung <noah227@foxmail.com>
2026-05-14 15:36:43 +0800

    chore: optimize popup sample page

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -4,15 +4,25 @@ console.log("This is popup!")
 
 <template>
 	<div id="popup">
+		<img src="/images/icon-48.png" alt="icon-48">
 		<strong>This is popup!</strong>
 	</div>
 </template>
 
+<style>
+#app {
+	width: fit-content;
+}
+</style>
 <style lang="scss" scoped>
 // sass already supported
 #popup {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 	> strong {
 		color: darkcyan;
+		white-space: nowrap;
 	}
 }
 </style>
